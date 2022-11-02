53f1f194d802c2788ed550ab5edb3cd85151d2fc
Vinicius Augusto <viniciguto1@gmail.com>
2022-11-02 15:31:12 -0300

    First commit

---
+++ laticineo-api/src/main/java/br/com/laticineoapi/LaticineoApplication.java
@@ -0,0 +1,13 @@
+package br.com.laticineoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaticineoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaticineoApplication.class, args);
+	}
+
+}
---
+++ laticineo-api/src/test/java/br/com/laticineoapi/LaticineoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.laticineoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaticineoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
