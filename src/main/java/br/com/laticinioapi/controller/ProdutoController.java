87bb735895c34f2edbb92b3e7f8efb7b831515a9
Vinicius Augusto <viniciguto1@gmail.com>
2022-11-16 18:05:14 -0300

    Save/ All/ update produto

---
+++ src/main/java/br/com/laticinioapi/controller/ProdutoController.java
@@ -0,0 +1,52 @@
+package br.com.laticinioapi.controller;
+
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import br.com.laticinioapi.entity.Produto;
+import br.com.laticinioapi.service.ProdutoService;
+
+@RestController
+@RequestMapping(value = "/produto")
+@CrossOrigin("*")
+public class ProdutoController {
+	
+	@Autowired
+	private ProdutoService produtoService;
+	
+	@PostMapping
+	public ResponseEntity<?> save(@RequestBody Produto produto) {
+
+		produtoService.save(produto);		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+							.path("/{id}").buildAndExpand(produto.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<Produto>> all() {
+		return ResponseEntity.status(HttpStatus.OK)
+				.body(produtoService.getAll());
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Void> update(@RequestBody Produto produto, @PathVariable Long id) {
+		produto.setId(id);
+		produtoService.update(produto);
+		return ResponseEntity.noContent().build();
+	}
+	
+}
---
+++ src/main/java/br/com/laticinioapi/service/ProdutoService.java
@@ -0,0 +1,36 @@
+package br.com.laticinioapi.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.laticinioapi.entity.Produto;
+import br.com.laticinioapi.repository.ProdutoRepository;
+
+@Service
+public class ProdutoService {
+
+	@Autowired
+	private ProdutoRepository produtoRepository;
+	
+	public Produto save(Produto produto) {
+		return produtoRepository.save(produto);
+	}
+	
+	public List<Produto> getAll(){
+		return produtoRepository.findAll();
+	}
+	
+	public Optional<Produto> findById(Long id){
+		return produtoRepository.findById(id);
+	}
+	
+	public void update(Produto produto) {
+		var upProduto = findById(produto.getId());
+		BeanUtils.copyProperties(produto, upProduto.get());
+		produtoRepository.save(upProduto.get());
+	}
+}
