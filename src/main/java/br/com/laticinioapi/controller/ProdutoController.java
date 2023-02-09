33eab81e8764b31fefeaa47de03f3e4af43ba35b
Vinicius Augusto <viniciguto1@gmail.com>
2023-02-08 21:48:17 -0300

    ProdutoController, teste de commit utilizando a IDE intellij

--- src/main/java/br/com/laticinioapi/controller/ProdutoController.java
+++ src/main/java/br/com/laticinioapi/controller/ProdutoController.java
@@ -1,66 +1,57 @@
 package br.com.laticinioapi.controller;
 
-import java.net.URI;
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
 import br.com.laticinioapi.dto.ProdutoDto;
 import br.com.laticinioapi.entity.Produto;
 import br.com.laticinioapi.service.ProdutoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/produto")
-//@CrossOrigin("https://emporiomineirinho.vercel.app/")
 @CrossOrigin("*")
 public class ProdutoController {
-	
-	@Autowired
-	private ProdutoService produtoService;
-	
-	@PostMapping
-	public ResponseEntity<?> save(@RequestBody Produto produto) {
 
-		produtoService.save(produto);		
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
-							.path("/{id}").buildAndExpand(produto.getId()).toUri();
-		return ResponseEntity.created(uri).build();
-	}
-	
-	@GetMapping
-	public ResponseEntity<List<Produto>> all() {
-		return ResponseEntity.status(HttpStatus.OK)
-				.body(produtoService.getAll());
-	}
-	
-	@PutMapping("/{id}")
-	public ResponseEntity<Void> update(@RequestBody Produto produto, @PathVariable Long id) {
-		produto.setId(id);
-		produtoService.update(produto);
-		return ResponseEntity.noContent().build();
-	}
-	
-	@GetMapping("/controle")
-	public ResponseEntity<List<ProdutoDto>> allQuantidade() {
-		return ResponseEntity.status(HttpStatus.OK)
-				.body(produtoService.getAllDto());
-	}
-	
-	@GetMapping("/list")
-	public ResponseEntity<List<Produto>> allListLimit() {
-		return ResponseEntity.status(HttpStatus.OK)
-				.body(produtoService.findByListLimit());
-	}
-	
+    @Autowired
+    private ProdutoService produtoService;
+
+    @PostMapping
+    public ResponseEntity<?> save(@RequestBody Produto produto) {
+
+        produtoService.save(produto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(produto.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Produto>> all() {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(produtoService.getAll());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> update(@RequestBody Produto produto, @PathVariable Long id) {
+        produto.setId(id);
+        produtoService.update(produto);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/controle")
+    public ResponseEntity<List<ProdutoDto>> allQuantidade() {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(produtoService.getAllDto());
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity<List<Produto>> allListLimit() {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(produtoService.findByListLimit());
+    }
+
 }
